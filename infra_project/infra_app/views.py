232372d6099b2f1d699e7b555bf4405b7ef752f9
Sofya Serpinskaya <sofyaserpinskaya@gmail.com>
2022-04-14 12:49:36 +0300

    new index page

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Обновленная страница!')
 
 
 def second_page(request):
